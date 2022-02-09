0fda08fe04312c82e7ee460c471f0cf085de447f
Timothy Gaither <timjgaither@gmail.com>
2022-02-09 16:32:53 +0100

    push esercizio

--- main.js
+++ main.js
@@ -8,8 +8,22 @@ const app= new Vue(
         data:{
            message:'Il mio primo messaggio su vue!!!!',
            img:'img/crash-bandicoot-4-its-about-time-store-art.jpg',
-           nome:"",
+           nome:'Crash-Bandicoot',
+           giochi:'Scrivi qualcosa'
         },
+        methods:{
+            cambiaImmagine:function(){
+                if(this.img=='img/crash-bandicoot-4-its-about-time-store-art.jpg' && this.nome== 'Crash-Bandicoot'){
+                    this.img='img/returnal-listing-thumb-01-ps5.jpg';
+                    this.nome='Returnal';
+                }else{
+                    this.img='img/crash-bandicoot-4-its-about-time-store-art.jpg';
+                    this.nome='Crash-Bandicoot';
+                }
+            },
+
+            
+        }
         
 }
 );
