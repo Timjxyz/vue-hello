d4f93f5f5592716706217a2117e1ab90fe3689c5
Timothy Gaither <timjgaither@gmail.com>
2022-02-09 14:49:57 +0100

    push esercizio

---
+++ main.js
@@ -0,0 +1,14 @@
+//Variabile che crea Vue
+const app= new Vue(
+    {
+        //Elemento base di vue
+        el:'#root',
+
+        //Variabili all'interno di vue
+        data:{
+           message:'Il mio primo messaggio su vue!!!!',
+           img:'pic',
+        },
+        
+}
+);
